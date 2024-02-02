56b5a3c66933d997c7674a7358335edf456e8672
isGavri <zssykes1@gmail.com>
2024-02-02 11:08:28 -0600

    Added gitignore to exclude executables and build carpet

---
+++ RequisitosTareas.cpp
@@ -0,0 +1,16 @@
+/* Presentación 
+Nomre: Sergio Franco Casillas
+Fecha: 23 de Enero de 2022
+Programa: holamundo.cpp
+CENTRO UNIVERSITARIO DE LOS ALTOS / UNIVERSIDAD DE GUADALAJARA
+INGENIERIA EN COMPUTACIÓN / 2DO SEMESTRE
+Profesor: Sergio Franco Casillas
+Descripción: Describir el contenido del archivo
+*/
+
+#include <iostream>
+ int main(){
+  std::cout.sync_with_stdio(false);
+  std::cout << "Hola" << std::endl;
+  return 0;
+}
