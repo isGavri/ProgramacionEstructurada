0319dd692773fc166a3c168bf2a73b8d26e36ad5
isGavri <zssykes1@gmail.com>
2024-02-02 12:01:50 -0600

    Added primeraactividad

---
+++ PrimeraActividad/PrimeraActividad.cpp
@@ -0,0 +1,16 @@
+/* Presentación 
+Nomre: Estefan Gabriel Partida Toalá
+Fecha: 23 de Enero de 2022
+Programa: RequisitosTareas.cpp
+CENTRO UNIVERSITARIO DE LOS ALTOS / UNIVERSIDAD DE GUADALAJARA
+INGENIERIA EN COMPUTACIÓN / 2DO SEMESTRE
+Profesor: Sergio Franco Casillas
+Descripción: Describir el contenido del archivo
+*/
+#include <iostream> //Libreria para inputs y outputs
+int main(){
+  std::cout.sync_with_stdio(false); //Tengo problemas con los buffers, y la terminal espera printf antes de los cout, esta funcion los desliga y me deja ejecutar 
+
+
+}
+
