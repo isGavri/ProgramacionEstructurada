96df6841e5a62b0c594b67ac05bc2fb0f94e2076
isGavri <zssykes1@gmail.com>
2024-02-07 21:41:26 -0600

    Added comments to CuartaActividad

--- RequisitosTareas.cpp
+++ RequisitosTareas.cpp
@@ -1,3 +1,7 @@
+/* Copyright (C) 1989 GNU
+ * Everyone is permitted to copy and distribute copies
+* of this license document, but changing it is not allowed
+*/
 /* Presentación 
 Nomre: Estefan Gabriel Partida Toalá
 Fecha: 6 de Febrero del 2024
@@ -5,15 +9,10 @@ Programa: CuartaActividad.cpp
 CENTRO UNIVERSITARIO DE LOS ALTOS / UNIVERSIDAD DE GUADALAJARA
 INGENIERIA EN COMPUTACIÓN / 2DO SEMESTRE
 Profesor: Sergio Franco Casillas
-Descripción: Elaborar la serie Fibonacci utilizando los 3 ciclos básicos (for, while y do-while)
-
-Solicitar al usuario un valor, y la serie debe llegar a un valor igual o menor al indicado por el usuario
-
-El usuario va a elegir con qué ciclo debe trabajar
-El programa debe salir cuando el usuario lo indique
+Descripción: Decripcion
 */
 #include <iostream>
- int main(){
+int main() {
   std::cout.sync_with_stdio(false);
   std::cout << "Hola" << std::endl;
   return 0;
